1c88ee0d22559dbfa5ae92b656e14f5d54cfdc67
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-26 17:00:57 +0700

    :100: Change category name to String

--- learn-mvc/learn-mvc/Models/Category.cs
+++ learn-mvc/learn-mvc/Models/Category.cs
@@ -7,7 +7,7 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
-    public int Name { get; set; }
+    public String Name { get; set; }
     public int DisplayOrder { get; set; }
 
     public DateTime CreateDateTime { get; set; } = DateTime.Now;
