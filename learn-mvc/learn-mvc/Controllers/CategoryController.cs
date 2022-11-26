9f658274330f471bbc841ca422bf1be327f427bf
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-11-26 19:25:09 +0700

    :+1: Get data to edit

--- learn-mvc/learn-mvc/Controllers/CategoryController.cs
+++ learn-mvc/learn-mvc/Controllers/CategoryController.cs
@@ -17,9 +17,50 @@ namespace learn_mvc.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        // GET
         public IActionResult Create()
         {
             return View();
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // SingleOrDefault throw an Error when there is more than 1 item returned
+            // FirstOrDefault will throw an Exception when there is more than 1 element
+            // Find will find something based on the ID primary key in the table
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        public IActionResult GetDetails(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
         }
     }
 }
