94bae7d123bd7cf2cce4b329571ef67a5dc3e0c6
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-12-15 15:03:25 +0700

    :+1: Update EF Core

--- learn-mvc/learn-mvc/Models/Category.cs
+++ learn-mvc/learn-mvc/Models/Category.cs
@@ -7,8 +7,11 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [StringLength(50)]
     public String Name { get; set; }
     public int DisplayOrder { get; set; }
 
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime CreateDateTime { get; set; } = DateTime.Now;
 }
