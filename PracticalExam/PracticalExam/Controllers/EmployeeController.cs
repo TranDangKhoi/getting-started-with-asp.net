82a234257c3375148460a2ec9d283d99321daef6
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-12-17 17:59:14 +0700

    :+1: Displayed all employees

---
+++ PracticalExam/PracticalExam/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticalExam.Data;
+
+namespace PracticalExam.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objEmployeeList = _db.Employees.ToList();
+            return View(objEmployeeList);
+        }
+    }
+}
